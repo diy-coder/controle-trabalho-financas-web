ce6151a0981055b5c1aad508046216afa3ac4e76
Arnaldo C. Bezerra <diy-coder@outlook.com>
2022-11-08 17:33:08 +0000

    Add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAN4BdYBlcobcWsYZ3HR-EZNaAm4LK-1yU",
+    authDomain: "controle-trabalho-financas.firebaseapp.com",
+    projectId: "controle-trabalho-financas",
+    storageBucket: "controle-trabalho-financas.appspot.com",
+    messagingSenderId: "445265390480",
+    appId: "1:445265390480:web:1298c56c1377cb21b66115",
+    measurementId: "G-RL4PLCVVSF"
+  }
 };
